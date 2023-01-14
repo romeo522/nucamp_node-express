465be381acc708798deb585147b2ac33ad56de57
romeo <romeorangeljr@gmail.com>
2023-01-14 11:15:44 -0600

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
+  res.statusCode = 200;
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
